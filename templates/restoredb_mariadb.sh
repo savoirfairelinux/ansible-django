96c49f20ac34dbd33f564823f6866fe3d247f38b
Virgil Dupras <virgil.dupras@savoirfairelinux.com>
2017-01-09 14:58:49 -0500

    Drop/create db before importing in restoredb_mariadb.sh
    Otherwise, we're not restoring to a clean DB and it causes problems.

--- templates/restoredb_mariadb.sh
+++ templates/restoredb_mariadb.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+mysql -u {{ django_dbuser }} --password="{{ django_dbpass }}" -e 'drop database {{ django_dbname }}; create database {{ django_dbname }};'
 mysql -u {{ django_dbuser }} --password="{{ django_dbpass }}" -D {{ django_dbname }} 
